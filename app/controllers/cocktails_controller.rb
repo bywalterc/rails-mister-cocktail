4b1b825983860b4d5bacd373328891677282d475
Walter Cegarra <walter.cegarra@icloud.com>
2019-07-27 14:25:58 +0100

    Seed and first methods for Cocktails

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,20 @@
+class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Seed ingredients
+lemon = Ingredient.new(name: "lemon")
+lemon.save
+ice = Ingredient.new(name: "ice")
+ice.save
+mint = Ingredient.new(name: "mint leaves")
+mint.save
+vodka = Ingredient.new(name: "vodka")
+vodka.save
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "prosecco")
+Ingredient.create(name: "champagne")
+Ingredient.create(name: "orange juice")
+Ingredient.create(name: "vermouth")
+Ingredient.create(name: "olives")
+
+# Seed cocktails
+Cocktail.create(name: "martini")
+mohito = Cocktail.new(name: "mohito")
+mohito.save
+
+# Seed doses
+dose_lemon = Dose.new(description: '2 slices', cocktail: mohito, ingredient: lemon)
+dose_lemon.save
+
+dose_vodka = Dose.new(description: '10 cl', cocktail: mohito, ingredient: vodka)
+dose_vodka.save
+
+dose_mint = Dose.new(description: '5 leaves', cocktail: mohito, ingredient: mint)
+dose_mint.save
+
+dose_ice = Dose.new(description: 'small handfull crushed', cocktail: mohito, ingredient: ice)
+dose_ice.save
